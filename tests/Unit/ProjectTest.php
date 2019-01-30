7468458ae234e17ecde90ee05d94c924d8e71614
Qing Ma <gsmofgsm@hotmail.com>
2019-01-30 21:13:58 +0100

    a project belongs to an owner

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -10,9 +11,16 @@ class ProjectTest extends TestCase
 {
     use RefreshDatabase;
     /** @test */
-    public function it_has_a_path()
+    public function a_project_has_a_path()
     {
         $project = factory('App\Project')->create();
         $this->assertEquals('/projects/' . $project->id, $project->path());
+    }
+
+    /** @test */
+    public function a_project_belongs_to_an_owner()
+    {
+        $project = factory('App\Project')->create();
+        $this->assertInstanceOf(User::class, $project->owner);
     }
 }
