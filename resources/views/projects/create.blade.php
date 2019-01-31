f2190cf73eb23b425a0427ce3f8b7f0e051832b7
Qing Ma <gsmofgsm@hotmail.com>
2019-01-31 09:27:21 +0100

    layout

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,46 +1,40 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
-</head>
-<body>
-<h1>Create a project</h1>
-<form method="POST" action="/projects">
-    @csrf
-    <div class="field">
+@extends('layouts.app')
+@section('content')
+    <h1>Create a project</h1>
+    <form method="POST" action="/projects">
+        @csrf
+        <div class="field">
 
-        <label class="label" for="title">Title</label>
+            <label class="label" for="title">Title</label>
 
-        <div class="control">
+            <div class="control">
 
-            <input type="text" class="input" id="title" name="title" placeholder="Title" value="">
+                <input type="text" class="input" id="title" name="title" placeholder="Title" value="">
+
+            </div>
 
         </div>
 
-    </div>
+        <div class="field">
 
-    <div class="field">
+            <label class="label" for="description">Description</label>
 
-        <label class="label" for="description">Description</label>
+            <div class="control">
 
-        <div class="control">
+                <input type="text" class="input" id="description" name="description" placeholder="description" value="">
 
-            <input type="text" class="input" id="description" name="description" placeholder="description" value="">
-
-        </div>
-
-    </div>
-    <div class="field">
-
-        <div class="control">
-
-            <button type="submit" class="button is-link" id="create" name="create">Create</button>
+            </div>
 
         </div>
+        <div class="field">
 
-    </div>
-</form>
+            <div class="control">
 
-</body>
-</html>
+                <button type="submit" class="button is-link" id="create" name="create">Create</button>
+                <a href="/projects">Cancel</a>
+
+            </div>
+
+        </div>
+    </form>
+@endsection
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,17 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-</head>
-<body>
-<ul>
-    @forelse($projects as $project)
-        <li>
-            <a href="{{ $project->path() }}">{{ $project->title }}</a>
-        </li>
-    @empty
-        <li>No project yet.</li>
-    @endforelse
-</ul>
-</body>
-</html>
+@extends('layouts.app')
+@section('content')
+    <div style="display:flex;">
+        <h1 style="margin-right:auto;">Birdboard</h1>
+        <a href="/projects/create">New project</a>
+    </div>
+    <ul>
+        @forelse($projects as $project)
+            <li>
+                <a href="{{ $project->path() }}">{{ $project->title }}</a>
+            </li>
+        @empty
+            <li>No project yet.</li>
+        @endforelse
+    </ul>
+@endsection
