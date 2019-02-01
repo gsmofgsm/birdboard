d42d9c608ebc7d113d57ab8bb46a1be2f70a2038
Qing Ma <gsmofgsm@hotmail.com>
2019-02-01 22:58:33 +0100

    show project page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <header class="flex items-center mb-3 py-4">
         <div class="flex justify-between items-end w-full">
-            <h1 class="text-grey text-sm font-normal">My Projects</h1>
+            <h2 class="text-grey text-sm font-normal">My Projects</h2>
             <a href="/projects/create" class="button">New project</a>
         </div>
     </header>
@@ -10,7 +10,7 @@
     <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse($projects as $project)
             <div class="lg:w-1/3 px-3 pb-6">
-                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px;">
+                <div class="card" style="height: 200px;">
                     <h3 class="font-normal text-xl py-4 border-l-4 border-blue-light -ml-5 mb-4 pl-4">
                         <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
                     </h3>
