0452ce7b1967fa4d2eee72d2a86686b90e1fd412
Qing Ma <gsmofgsm@hotmail.com>
2019-02-06 22:23:09 +0100

    add a task UI

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -18,6 +18,12 @@
                     @foreach($project->tasks as $task)
                         <div class="card mb-3">{{ $task->body }}</div>
                     @endforeach
+                    <div class="card mb-3">
+                        <form action="{{ $project->path() . '/tasks' }}" method="POST">
+                            @csrf
+                            <input name="body" class="w-full" placeholder="Add a task...">
+                        </form>
+                    </div>
                 </div>
 
                 <div>
