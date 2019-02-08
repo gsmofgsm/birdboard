4b0f3a4fe014fc7bbb7d536e6cec47db2a1e6a13
Qing Ma <gsmofgsm@hotmail.com>
2019-02-08 23:08:16 +0100

    css create project page

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,36 +1,57 @@
 @extends('layouts.app')
 @section('content')
     <h1>Create a project</h1>
-    <form method="POST" action="/projects">
+    <form
+        method="POST"
+        action="/projects"
+        class="lg:w-1/2 lg:mx-auto bg-white p-6 md:py-12 rounded shadow"
+    >
         @csrf
-        <div class="field">
 
-            <label class="label" for="title">Title</label>
+        <h1 class="text-2xl font-normal mb-10 text-center">
+            Let's start something new
+        </h1>
+
+        <div class="field mb-6">
+
+            <label class="label text-sm mb-2 block" for="title">Title</label>
 
             <div class="control">
 
-                <input type="text" class="input" id="title" name="title" placeholder="Title" value="">
+                <input
+                    type="text"
+                    class="input bg-transparent border border-grey-light rounded p-2 text-xs w-full"
+                    id="title"
+                    name="title"
+                    placeholder="Title"
+                    value="">
 
             </div>
 
         </div>
 
-        <div class="field">
+        <div class="field mb-6">
 
-            <label class="label" for="description">Description</label>
+            <label class="label text-sm mb-2 block" for="description">Description</label>
 
             <div class="control">
 
-                <input type="text" class="input" id="description" name="description" placeholder="description" value="">
+                <input
+                    type="text"
+                    class="input bg-transparent border border-grey-light rounded p-2 text-xs w-full"
+                    id="description"
+                    name="description"
+                    placeholder="description"
+                    value="">
 
             </div>
 
         </div>
         <div class="field">
 
-            <div class="control">
+            <div class="control flex">
 
-                <button type="submit" class="button is-link" id="create" name="create">Create</button>
+                <button type="submit" class="button is-link mr-auto" id="create" name="create">Create</button>
                 <a href="/projects">Cancel</a>
 
             </div>
