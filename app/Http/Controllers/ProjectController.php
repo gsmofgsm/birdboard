135e0184b4f502582cfc46d5e7c0bbfc92311e3c
Qing Ma <gsmofgsm@hotmail.com>
2019-02-09 12:09:41 +0100

    authorization on edit project page

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -36,6 +36,8 @@ class ProjectController extends Controller
 
     public function edit(Project $project)
     {
+        $this->authorize('update', $project);
+        
         return view('projects.edit', compact('project'));
     }
 
