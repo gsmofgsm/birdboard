fdc419f9830e8b2542bd41707181aae6200ed2fd
Qing Ma <gsmofgsm@hotmail.com>
2019-02-09 12:03:53 +0100

    Validation errors feedback

--- resources/views/projects/_form.blade.php
+++ resources/views/projects/_form.blade.php
@@ -45,3 +45,11 @@
     </div>
 
 </div>
+
+ @if($errors->any())
+     <div class="field mt-6">
+         @foreach($errors->all() as $error)
+             <li class="text-sm text-red">{{ $error }}</li>
+         @endforeach
+    </div>
+ @endif
