8c164180995b550152ef3c5775ca940cbcf15052
Qing Ma <gsmofgsm@hotmail.com>
2019-02-13 23:13:03 +0100

    extract activity feeds to its own view

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -63,15 +63,7 @@
 
             <div class="lg:w-1/4 px-3 pt-8">
                 @include('projects.card')
-                <div class="card mt-3">
-                    <ul class="text-xs list-reset">
-                        @foreach( $project->activity as $activity )
-                            <li class="{{ $loop->last ? '' : 'mb-1' }}">
-                                @include( "projects.activity.{$activity->description}" )
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
+                @include('projects.activity.card')
             </div>
         </div>
     </main>
