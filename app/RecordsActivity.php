0fc2d44858b73ebeed0d54acc51ac6800f6f1860
Qing Ma <gsmofgsm@hotmail.com>
2019-02-15 21:49:32 +0100

    move observe triggering event from observer to RecordsActivity trait

--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -3,6 +3,27 @@ namespace App;
 
 trait RecordsActivity
 {
+    public static function bootRecordsActivity()
+    {
+        foreach(self::recordableEvents() as $event) {
+            static::$event(function($model) use ($event) {
+                $event_description = class_basename($model) === 'Project' ? $event : strtolower(class_basename($model)) . '_' . $event;
+                $model->registerActivity($event_description);
+            });
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public static function recordableEvents(): array
+    {
+        if (isset(static::$recordableEvents)) {
+            return static::$recordableEvents;
+        }
+        return ['created', 'updated', 'deleted'];
+    }
+
     public function registerActivity($description)
     {
         $changes = $this->activityChanges();
--- app/Task.php
+++ app/Task.php
@@ -16,6 +16,8 @@ class Task extends Model
         'completed' => 'boolean'
     ];
 
+    protected static $recordableEvents = ['created', 'deleted'];
+
     public function project()
     {
         return $this->belongsTo(Project::class);
