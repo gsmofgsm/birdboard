0132e8c01bb8f6007460d7752b47963f065363f3
Qing Ma <gsmofgsm@hotmail.com>
2019-02-21 22:38:45 +0100

    delete project button on project list page

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -2,5 +2,12 @@
     <h3 class="font-normal text-xl py-4 border-l-4 border-blue-light -ml-5 mb-4 pl-4">
         <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
     </h3>
-    <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+    <div class="text-grey mb-4">{{ str_limit($project->description, 100) }}</div>
+    <footer>
+        <form action="{{ $project->path() }}" method="POST" class="text-right">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="text-xs">Delete</button>
+        </form>
+    </footer>
 </div>
