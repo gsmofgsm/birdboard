ae496523ed0c2607a1a8bb671cc294a499bd4058
Qing Ma <gsmofgsm@hotmail.com>
2019-03-09 23:27:13 +0100

    move gravatar_url() to helpers.php

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,6 @@
 |
 */
 
-function gravatar_url($email) {
-    $email = md5($email);
-    return "https://gravatar.com/avatar/{$email}?s=60";
-}
-
 Route::get('/', function () {
     return view('welcome');
 });
